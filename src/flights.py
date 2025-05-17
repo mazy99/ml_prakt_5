6bbfe9d7ec331684bf33faba2d5f49e8fc2e2f69
mazy99 <mazy.1999@mail.ru>
2025-05-17 21:30:53 +0300

    fix number flights

--- src/flights.py
+++ src/flights.py
@@ -1,12 +1,15 @@
 import pandas as pd
-from src.validators import validate_flight
-from src.storage import save_to_parquet
+from validators import validate_flight
+from storage import save_to_parquet
 
 def add_flight(df, destination, flight_number, aircraft_type):
     if not validate_flight(destination, flight_number, aircraft_type):
         return df
 
     flight_number = str(flight_number).strip()
+    if flight_number in df["flight_number"].values:
+        print(f"❌ Ошибка: Рейс с номером {flight_number} уже существует!")
+        return df  # Возвращаем исходный DataFrame без изменений
     new_data = pd.DataFrame([{
         "destination": destination.strip(),
         "flight_number": flight_number,
